4ff448d0ba6573c4dd1d76765e18c116562b1c9f
rahul ojha <rojha@grepruby.com>
2018-08-03 15:22:37 +0530

    Add webpacker setup

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,16 @@
 const { environment } = require('@rails/webpacker')
 
+
+environment.loaders.get('sass').use
+  .find(item => item.loader === 'sass-loader')
+  .options.includePaths = ['node_modules', 'node_modules/datatables.net-dt'];
+
+// resolve-url-loader must be used before sass-loader
+environment.loaders.get('sass').use.splice(-1, 0, {
+  loader: 'resolve-url-loader',
+  options: {
+    attempts: 1
+  }
+});
+
 module.exports = environment
